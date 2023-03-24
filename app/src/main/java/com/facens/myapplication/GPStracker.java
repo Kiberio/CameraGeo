837b6da1e8430ce5a6555860e57ed5b296f5993c
Daniel Hiroshi Fugikawa <dhiroshi3@gmail.com>
2023-03-24 01:14:18 -0300

    Update

--- app/src/main/java/com/facens/myapplication/GPStracker.java
+++ app/src/main/java/com/facens/myapplication/GPStracker.java
@@ -14,42 +14,42 @@ import androidx.core.content.ContextCompat;
 
 public class GPStracker implements LocationListener {
     Context context;
-    //
+    //Criação de um construtor para a classe GPStracker
     public GPStracker(Application c){
         context = c;
     }
-    //
+    //Cria uma função para adiquirir a localização do usuario
     public Location getLocation(){
-        //
+        //if para verificar se o aplicativo possui a permissão ao GPS do dispositivo
         if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED){
-            //
-            Toast.makeText(context, "Não foi permitir", Toast.LENGTH_SHORT).show();
+            //Caso não tenha permissão avisa que não tem a permissão
+            Toast.makeText(context, "Não foi permitido", Toast.LENGTH_SHORT).show();
             return null;
         }
 
 
         LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        //
+        //Caso tenha acesso ao Gps guarda a localização na variavel l e retorna o valor para imprimir no MainActivity
         if (isGPSEnabled){
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
             Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             return l;
         } else {
-            //
-            Toast.makeText(context, "Por favor, habitar o GPS!", Toast.LENGTH_LONG).show();
+            //Caso o usuario esteja tentando usar a função do GPS com o GPS desligado
+            Toast.makeText(context, "Por favor, habilitar o GPS!", Toast.LENGTH_LONG).show();
         }
-        //
+        //Reseta o valor
         return null;
     }
-    //
+    //Reescreve a variavel onProviderDisabled caso mude de estado
     @Override
     public void onProviderDisabled(@NonNull String provider) {    }
-    //
+    //Reescreve o valor da variavel caso mude a localização
     @Override
     public void onLocationChanged(@NonNull Location location) {    }
-    //
+    //Reescreve o status do provedor de locaização
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {    }
 }
--- app/src/main/java/com/facens/myapplication/Splash.java
+++ app/src/main/java/com/facens/myapplication/Splash.java
@@ -9,15 +9,15 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
-    //
+    //Criação de uma variavel timer
     private final Timer timer = new Timer();
     TimerTask timerTask;
-    //
+    //Criação da tela activity_Splash quando abre o aplicativo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //
+        //Quando passar o tempo do TimerTask mostrar a tela principal do aplicativo
         timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -31,10 +31,10 @@ public class Splash extends AppCompatActivity {
 
                 }
             };
-        //
+        //Declaração do tempo do TimerTask
         timer.schedule(timerTask, 3000);
         }
-        //
+        //função para ir para a tela principal do aplicativo
         private void gotoMainActivity(){
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
